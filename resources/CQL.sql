14a7171e3cea3d82c9e076d81c15ca23b6d2f408
rickvig <rickuev@gmail.com>
2022-07-10 23:41:23 -0300

    Exemplos de conexão do Cassandra e Neo4j

---
+++ resources/CQL.sql
@@ -0,0 +1,12 @@
+CREATE TABLE rh.cities
+(
+    id int,
+    name text,
+    country text,
+    PRIMARY KEY(id)
+);
+
+INSERT INTO teste.cities
+    (id,name,country)
+VALUES
+    (1, 'Karachi', 'Pakistan');
